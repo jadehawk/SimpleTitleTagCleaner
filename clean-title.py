28b6b21651c80eca561fdf26716839c060958f0f
Jadehawk <333657+jadehawk@users.noreply.github.com>
2023-07-20 21:32:58 -0400

    Create clean-title.py

---
+++ clean-title.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+print("SABnzbd version:", os.environ['SAB_VERSION'])
+print("Job location:", os.environ['SAB_COMPLETE_DIR'])
+print("Fail msg:", os.environ['SAB_FAIL_MSG'])
+
+# Your code
+
+# Success code
+sys.exit(0)
