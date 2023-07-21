60a5dc7513c68f4b21c1a15972e410f5ace9ca42
Jadehawk <333657+jadehawk@users.noreply.github.com>
2023-07-21 03:46:58 -0400

    Update clean-title.py

--- clean-title.py
+++ clean-title.py
@@ -5,8 +5,9 @@ print("SABnzbd version:", os.environ['SAB_FINAL_NAME'])
 print("Job location:", os.environ['SAB_FILENAME'])
 print("Path:", os.environ['SAB_COMPLETE_DIR	'])
 
+filename = (os.environ['SAB_COMPLETE_DIR'] / *.*)
 
-# Your code
+print(filename)
 
 # Success code
 sys.exit(0)
