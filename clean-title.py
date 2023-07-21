704dcdb87f0179206069aa1597c561c2246fc072
Jadehawk <333657+jadehawk@users.noreply.github.com>
2023-07-21 03:50:59 -0400

    Update clean-title.py

--- clean-title.py
+++ clean-title.py
@@ -1,13 +1,2 @@
-import os
-import sys
-
-print("SABnzbd version:", os.environ['SAB_FINAL_NAME'])
-print("Job location:", os.environ['SAB_FILENAME'])
-print("Path:", os.environ['SAB_COMPLETE_DIR	'])
-
-filename = (os.environ['SAB_COMPLETE_DIR'] / *.*)
-
-print(filename)
-
-# Success code
-sys.exit(0)
+#!bin/bash
+print $1 $2 $3 $4 $5 $6 $7 $8
