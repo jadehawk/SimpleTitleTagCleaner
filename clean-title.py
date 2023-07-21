d01a1e7f36a375cc57d0c7987c86b52e18ea72b2
Jadehawk <333657+jadehawk@users.noreply.github.com>
2023-07-21 03:35:07 -0400

    Update clean-title.py

--- clean-title.py
+++ clean-title.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-print("SABnzbd version:", os.environ['SAB_VERSION'])
-print("Job location:", os.environ['SAB_COMPLETE_DIR'])
-print("Fail msg:", os.environ['SAB_FAIL_MSG'])
-echo $1 $2 $3 $4 $5 $6 $7 $8
+print("SABnzbd version:", os.environ['SAB_FINAL_NAME'])
+print("Job location:", os.environ['SAB_FILENAME'])
+print("Fail msg:", os.environ['SAB_ORIG_NZB_GZ'])
+
 
 # Your code
 
